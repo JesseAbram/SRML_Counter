0772cc5ab9f34b586d7ed5110ff8d9094687a38d
Jesse Abramowitz <jesseabramowitz@hotmail.com>
2019-04-22 14:07:13 -0400

    demo

--- getset/runtime/src/demo.rs
+++ getset/runtime/src/demo.rs
@@ -1,13 +1,10 @@
-// ACTION: Update StorageValue to StorageMap to support mappings
 use support::{decl_storage, decl_module, StorageValue, dispatch::Result};
 use system::ensure_signed;
 
-// ACTION: Update this to use `balances::Trait` to access T::AccountId
 pub trait Trait: system::Trait {}
 
 decl_storage! {
     trait Store for Module<T: Trait> as Counter {
-        // ACTION: Update this storage item to be a `map` from `T::AccountId` to `u64`
         Count get(count): u64;
     }
 }
@@ -22,7 +19,6 @@ decl_module! {
 
             let newCount = Currentcount.wrapping_add(value);
             
-            // ACTION: Update this to `insert()` the key/value pair (sender, value)
             <Count<T>>::put(newCount);
 
             Ok(())
